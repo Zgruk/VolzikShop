1c9bc0e675056c33d6b99c454ceb1b6d5138c68a
Antoine <anton.pukhart@gmail.com>
2022-09-25 18:41:36 +0300

    added add button on detail page

--- cart/forms.py
+++ cart/forms.py
@@ -6,7 +6,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 6)]
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
                     choices=PRODUCT_QUANTITY_CHOICES,
-                    coerce=int)
+                    coerce=int,
+                    label='количество')
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
