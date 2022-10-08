7033a02437a692a46e337f0343c7b9c68edb7db2
Antoine <anton.pukhart@gmail.com>
2022-10-08 18:42:53 +0300

    'export to csv' were added to adminsite for orders

--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,15 @@
+from tabnanny import verbose
 from django.db import models
 from shop.models import Product
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50, verbose_name="Ваше имя")
+    last_name = models.CharField(max_length=50, verbose_name='Ваша фамилия')
+    email = models.EmailField(verbose_name='Адрес электронной почты')
+    address = models.CharField(max_length=250, verbose_name='Адрес')
+    postal_code = models.CharField(max_length=20, verbose_name='Почтовый индекс')
+    city = models.CharField(max_length=100, verbose_name='Город')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
